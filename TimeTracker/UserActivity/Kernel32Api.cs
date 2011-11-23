52d5137cd9d03428292348206254313e2f4b39da
Paul Clements <paulcclements@gmail.com>
2011-11-22 20:08:27 -0600

    Fixed Issue with .Net 4.0 SetWindowHookEx On 32 bit XP

---
+++ TimeTracker/UserActivity/Kernel32Api.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace UserActivity
+{
+	internal interface IKernel32
+	{
+		IntPtr LoadDll(string name);
+	}
+	
+	internal class Kernel32Api : IKernel32
+	{
+		public IntPtr LoadDll(string name)
+		{
+			return LoadLibrary(name);
+		}
+		
+		[DllImport("kernel32.dll")]
+        private static extern IntPtr LoadLibrary(string dllToLoad);
+	}
+}
